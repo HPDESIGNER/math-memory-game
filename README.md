2fe4c92e09a5d591f85258454a32814e1ecb7dae
Eugen <39004154+HPDESIGNER@users.noreply.github.com>
2019-07-05 07:45:00 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# to install
+npm install
+
+# running local / production  
+npm run serve
+npm run build
---
+++ src/vues/demo-component.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{content.groupListEntry1}} ||
+        {{content.groupListEntry2}} ||
+        {{content.groupListEntry3}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "demo-component",
+        props: {
+            content: {
+                type: Object
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
